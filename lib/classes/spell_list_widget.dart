95b3fc725d354ebcfea9800c99dfcfb49e6e5368
nicolas.franco <nicolas.franco@etu.uca.fr>
2023-02-19 13:03:11 +0100

    Pierre: spell_list_widget :sparkles:

---
+++ lib/classes/spell_list_widget.dart
@@ -0,0 +1,30 @@
+import 'package:code/classes/spell_detail_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SpellList extends StatelessWidget {
+  final List<Map<String, dynamic>> spells;
+
+  SpellList({required this.spells});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: spells.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(spells[index]['name']),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SpellDetailsPage(spell: spells[index]),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
