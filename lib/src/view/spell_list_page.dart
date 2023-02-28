6866e321c03d91691f96c4616052008686b59d89
lou.broda <lou.broda@etu.uca.fr>
2023-02-28 21:30:21 +0100

    fluid scroll for list view :zap:

--- lib/src/view/spell_list_page.dart
+++ lib/src/view/spell_list_page.dart
@@ -115,27 +115,12 @@ class _SpellListPage extends State<SpellListPage> {
         ],
       ),
       body: ListView.builder(
+        itemExtent: 50,
+        cacheExtent: 2,
         itemCount: spells_list.length,
         itemBuilder: (context, index) {
           return ListTile(
-            //leading: Image.file(File('./src/LuvLimule.png')),
-            /*leading: const Image(
-              image: AssetImage('assets/src/Sprite-SlimeGHallow.png'),
-              height: 150,
-            ),*/
-            leading: ExcludeSemantics(
-              //child: CircleAvatar(child: Text('$index')), //cercle avec index => numero dans la liste
-              child: CircleAvatar(
-                  child: Text(spells_list[index]
-                      .GetLevelByClass(character.cclass)
-                      .toString())), //! Changer le dum.cclass
-            ),
             title: Text(spells_list[index].name),
-            trailing: const Icon(
-              Icons.arrow_forward,
-              color: Colors.blueGrey,
-              size: 24.0,
-            ),
             onTap: () {
               Navigator.push(
                 context,
@@ -151,27 +136,27 @@ class _SpellListPage extends State<SpellListPage> {
     );
   }
 
-/* OLD WIDGET VIEW LIST 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Spell Names'),
-      ),
-      body: Container(
-          child: ListView.separated(
-              separatorBuilder: (context, index) =>
-                  const Divider(height: 0.5, color: Colors.black38),
-              physics: ClampingScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: spells_list == null ? 0 : spells_list.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                      '${spells_list[index].name} ${spells_list[index].school} ${spells_list[index].source}'),
-                );
-              })),
-    );
-  } */
+  /* OLD WIDGET VIEW LIST 
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Spell Names'),
+        ),
+        body: Container(
+            child: ListView.separated(
+                separatorBuilder: (context, index) =>
+                    const Divider(height: 0.5, color: Colors.black38),
+                physics: ClampingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: spells_list == null ? 0 : spells_list.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.all(15),
+                    child: Text(
+                        '${spells_list[index].name} ${spells_list[index].school} ${spells_list[index].source}'),
+                  );
+                })),
+      );
+    } */
 }
