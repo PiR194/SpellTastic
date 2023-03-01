dfe90e5ca2a8df08f3ffd67b8c5900be6c8f8bbf
nicolas.franco <nicolas.franco@etu.uca.fr>
2023-03-01 06:38:53 +0100

    started scrapping :mag:

---
+++ lib/scrapping/spells.py
@@ -0,0 +1,114 @@
+import urllib.request 
+import requests
+import bs4
+from bs4 import BeautifulSoup
+from lxml import html
+
+### -------------------------------------
+# GET ALL THE SPELLS FROM THE PAGE
+### -------------------------------------
+
+## GET <li> ELEMENTS NAME + URL TO DETAIL PAGE
+# url with all spells
+URL = "https://www.d20pfsrd.com/magic/all-spells/"
+
+# get the page content using GET to url
+response = requests.get(URL)
+
+# parse html using
+soup = BeautifulSoup(response.content, 'lxml')
+
+# i printed the content just to check
+# print(soup)
+
+# get all spells
+list = soup.find(id='article-content').find_next('div',class_="flexbox")
+#print(list)
+
+#uls = list.find_all('ul')
+#print(uls)
+
+# this gets all the <li> elements from the article-content div, which contain all of the 
+# spells (name and link to detail page)
+lis = list.find_all('li')
+# print(lis)
+
+# checked by writting names on a file
+#with open('spellzz.txt', 'w') as f:
+#    for li in lis:
+#        text = li.a.text
+#        f.write(text + '\n')
+
+## GET ALL DETAILS FROM EACH SPELL
+for li in lis:
+    url = li.a['href']
+    #print(url)
+
+    ## get html of details page
+    responseDetails = requests.get(url)
+    spellSoup = BeautifulSoup(responseDetails.content, 'lxml')
+
+    # get article content which contains all info about spells
+    spellContent = spellSoup.find(id='article-content')
+#    print(spellContent) # check if good
+
+    # get name
+    spell_name = spellContent.find('h1').text
+    print("name: ",spell_name)
+
+    # get school
+    spell_school = spellContent.find('b', text='School').find_next_sibling('a').text.strip()
+    print("school: ", spell_school)
+
+    # get level
+    levels = spellContent.find('b', text='Level').find_next_siblings('a')
+    #print("level: ", spell_level)
+
+    spell_level = []
+    for i in levels : 
+        if i.name == 'a':
+            spell_level.append(i.text.strip())
+
+    # print(spell_level) # check if gutten
+
+    # get casting time
+    spell_castTime = spellContent.find('b', text='Casting Time').next_sibling.strip() # nao ta em balisa entao next_sibling sem parenthesis como um attributo
+    print("cast time: ", spell_castTime)
+
+    # get components 
+    components = []
+    spell_components = spellContent.find('b', text='Components')#.next_sibling.strip()
+    if spell_components:
+        for sibling in spell_components.next_siblings:
+            if sibling.name == 'p': # if end of p tag
+                break
+            if sibling.name == 'a': # if href we want to just get text
+                spell_components.append(sibling.text)
+            elif isinstance(sibling, bs4.element.NavigableString): # if its just random a** text in no specific <tag>
+                component_text = sibling.string.strip()
+                if component_text:
+                    spell_components.append(component_text)
+    spell_components = ', '.join(spell_components) # final touch :chef's_kiss:
+    print(spell_components)
+
+    # get range
+    range_tag = spellContent.find('b', text='Range')
+    if range_tag:
+        try:
+            spell_range = range_tag.find_next_sibling('a').text.strip()
+        except AttributeError: # In case there isn't a RANGE section at all ------------- ! 
+            spell_range = range_tag.next_sibling.strip()
+    else:
+        spell_range = None
+    print("Range: ", spell_range)
+
+
+    exit(0)
+
+
+    # print(components) # output like: "V, S, M (a" # IF NOTHING NEXT TO "(a" REMOVE LATER ------------ !
+
+
+
+
+
