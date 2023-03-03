6cb260b7b4e3ef9e48552f522f38a3f7496240d1
lou.broda <lou.broda@etu.uca.fr>
2023-03-03 10:35:11 +0100

    fix all on swipe + add arrow to swipe

--- lib/src/view/setDisplay.dart
+++ lib/src/view/setDisplay.dart
@@ -2,27 +2,80 @@ import 'package:flutter/material.dart';
 import 'widgets/spellSetWidget.dart';
 import 'home.dart';
 
-class SetDisplay extends StatelessWidget {
+class SetDisplay extends StatefulWidget {
+  @override
+  _SetDisplayState createState() => _SetDisplayState();
+}
+
+class _SetDisplayState extends State<SetDisplay> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _goToPage(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final PageController setController = PageController();
     return Scaffold(
       appBar: AppBar(
         title: Text('Nom du set'),
         backgroundColor: Colors.black,
       ),
-      body: PageView(
-        controller: setController,
-        children: <Widget>[
-          Container(
-            child: SpellSetWidget(
-              level: '1',
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (int page) {
+                setState(() {
+                  _currentPage = page;
+                });
+              },
+              children: <Widget>[
+                Container(
+                  child: SpellSetWidget(
+                    level: '1',
+                  ),
+                ),
+                Container(
+                  child: SpellSetWidget(
+                    level: '2',
+                  ),
+                ),
+              ],
             ),
           ),
-          Container(
-            child: SpellSetWidget(
-              level: '2',
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: _currentPage == 0
+                    ? null
+                    : () {
+                        _goToPage(_currentPage - 1);
+                      },
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: _currentPage == 1
+                    ? null
+                    : () {
+                        _goToPage(_currentPage + 1);
+                      },
+              ),
+            ],
           ),
         ],
       ),
