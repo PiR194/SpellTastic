da5247886500abc1158b4d7190f3bb63715b5daf
Corentin "Koroh" RICHARD <corentin.richard@etu.uca.fr>
2023-03-03 10:15:38 +0100

    Adding unit tests

--- lib/src/model/spell.dart
+++ lib/src/model/spell.dart
@@ -3,27 +3,51 @@ import 'package:collection/collection.dart';
 class Spell {
   // Setting id
   int _id;
+  int get id => _id;
 
   String _name;
   String get name => _name;
+  set name(String name) => _name = name;
 
   String _description;
   String get description => _description;
+  set description(String description) => _description = description;
+
   String _reference;
-  // Setting source
+  String get reference => _reference;
+  set reference(String reference) => _reference = reference;
+
   String _source;
   String get source => _source;
+  set source(String source) => _source = source;
 
   String _school;
   String get school => _school;
+  set school(String school) => _school = school;
 
   Map<String, int> _level;
-  String _castingTime;
-  List<String> _components;
-  String _range;
-  String _target;
-  String _duration;
+  Map<String, int> get level => _level;
+  set level(Map<String, int> level) => _level = level;
 
+  String _castingTime;
+  String get castingTime => _castingTime;
+  set castingTime(String castingTime) => _castingTime = castingTime;
+
+  List<String> _components;
+  List<String> get components => _components;
+  set components(List<String> components) => _components = components;
+
+  String _range;
+  String get range => _range;
+  set range(String range) => _range = range;
+
+  String _target;
+  String get target => _target;
+  set target(String target) => _target = target;
+
+  String _duration;
+  String get duration => _duration;
+  set duration(String duration) => _duration = duration;
   Spell(
       this._id,
       this._name,
@@ -50,7 +74,29 @@ class Spell {
 
   @override
   String toString() {
-    return " id: $_id \nname: $_name \nschool: $_school";
+    var levelString = "";
+    _level.forEach((key, value) {
+      levelString += "$key: $value, ";
+    });
+    //levelString = levelString.substring(0, levelString.length - 2);*/
+
+    var componentsString = "";
+    _components.forEach((element) {
+      componentsString += "$element,";
+    });
+
+    return "id: $_id\n"
+        "name: $_name\n"
+        "description: $_description\n"
+        "reference: $_reference\n"
+        "source: $_source\n"
+        "school: $_school\n"
+        "level: {$levelString}\n"
+        "casting time: $_castingTime\n"
+        "components: [$componentsString]\n"
+        "range: $_range\n"
+        "target: $_target\n"
+        "duration: $_duration\n";
   }
 
   bool equals(Spell other) {
--- test/spell_test.dart
+++ test/spell_test.dart
@@ -1,5 +1,5 @@
 import 'package:test/test.dart';
-import '../lib/src/model/spell.dart';
+import 'package:code/src/model/spell.dart';
 
 void main() {
   Spell mainSpell = Spell(
@@ -9,7 +9,7 @@ void main() {
       "mainRef",
       "mainSource",
       "mainSchool",
-      Map<String, int>(),
+      <String, int>{},
       "mainCastTime",
       List.empty(),
       "mainRange",
@@ -22,7 +22,7 @@ void main() {
       "sideRef",
       "sideSource",
       "sideSchool",
-      Map<String, int>(),
+      <String, int>{},
       "sideCastTime",
       List.empty(),
       "sideRange",
@@ -35,14 +35,153 @@ void main() {
       "mainRef",
       "mainSource",
       "mainSchool",
-      Map<String, int>(),
+      <String, int>{},
       "mainCastTime",
       List.empty(),
       "mainRange",
       "mainTarget",
       "mainDuration");
+  group('Equal', () {
+    test('both Spell should be equal', () {
+      expect(copySpell.equals(mainSpell), true);
+    });
 
-  test('both Spell should be equal', () {
-    expect(copySpell.equals(mainSpell), true);
+    test('both Spell should not be equal', () {
+      expect(sideSpell.equals(mainSpell), false);
+    });
+  });
+
+  group('toString', () {
+    test('mainSpell.toString', () {
+      expect(
+          mainSpell.toString(),
+          equals(
+              'id: 1\nname: mainSpell\ndescription: mainDesc\nreference: mainRef\nsource: mainSource\nschool: mainSchool\nlevel: {}\ncasting time: mainCastTime\ncomponents: []\nrange: mainRange\ntarget: mainTarget\nduration: mainDuration\n'));
+    });
+    test('sideSpell.toString', () {
+      expect(
+          sideSpell.toString(),
+          equals(
+              'id: 2\nname: sideSpell\ndescription: sideDesc\nreference: sideRef\nsource: sideSource\nschool: sideSchool\nlevel: {}\ncasting time: sideCastTime\ncomponents: []\nrange: sideRange\ntarget: sideTarget\nduration: sideDuration\n'));
+    });
+    test('copySpell.toString', () {
+      expect(
+          copySpell.toString(),
+          equals(
+              'id: 1\nname: mainSpell\ndescription: mainDesc\nreference: mainRef\nsource: mainSource\nschool: mainSchool\nlevel: {}\ncasting time: mainCastTime\ncomponents: []\nrange: mainRange\ntarget: mainTarget\nduration: mainDuration\n'));
+    });
+  });
+
+  group('Constructor and Getter', () {
+    final spell = Spell(
+        1,
+        "Fireball",
+        "A bright streak flashes from your pointing finger to a point you choose within range then blossoms with a low roar into an explosion of flame.",
+        "PHB",
+        "Wizard spell list",
+        "Evocation",
+        {"Wizard": 3, "Sorcerer": 3},
+        "1 action",
+        ["V", "S", "M (a tiny ball of bat guano and sulfur)"],
+        "150 feet",
+        "20-foot-radius sphere",
+        "Instantaneous");
+
+    test('Name', () {
+      expect(spell.name, equals("Fireball"));
+    });
+    test('Description', () {
+      expect(
+          spell.description,
+          equals(
+              "A bright streak flashes from your pointing finger to a point you choose within range then blossoms with a low roar into an explosion of flame."));
+    });
+    test('Source', () {
+      expect(spell.source, equals("Wizard spell list"));
+    });
+    test('School', () {
+      expect(spell.school, equals("Evocation"));
+    });
+    test('Level', () {
+      expect(spell.level, equals({"Wizard": 3, "Sorcerer": 3}));
+    });
+    test('Component', () {
+      expect(spell.components,
+          equals(["V", "S", "M (a tiny ball of bat guano and sulfur)"]));
+    });
+    test('Casting Time', () {
+      expect(spell.castingTime, equals("1 action"));
+    });
+    test('Range', () {
+      expect(spell.range, equals("150 feet"));
+    });
+    test('Target', () {
+      expect(spell.target, equals("20-foot-radius sphere"));
+    });
+    test('Duration', () {
+      expect(spell.duration, equals("Instantaneous"));
+    });
+  });
+
+  group('Setter', () {
+    final spell = Spell(
+        1,
+        "Cure Wounds",
+        "A creature you touch regains a number of hit points equal to 1d8 + your spellcasting ability modifier. This spell has no effect on undead or constructs.",
+        "PHB",
+        "Cleric spell list",
+        "Evocation",
+        {"Cleric": 1},
+        "1 action",
+        ["V", "S"],
+        "Touch",
+        "1 creature",
+        "Instantaneous");
+
+    spell.name = "a";
+    test('Name', () {
+      expect(spell.name, equals("a"));
+    });
+
+    spell.description = "a";
+    test('Descritpion', () {
+      expect(spell.description, equals("a"));
+    });
+    spell.description = "a";
+    test('Reference', () {
+      expect(spell.reference, equals("PHB"));
+    });
+    spell.description = "a";
+    test('Source', () {
+      expect(spell.source, equals("Cleric spell list"));
+    });
+    spell.description = "a";
+    test('School', () {
+      expect(spell.school, equals("Evocation"));
+    });
+    spell.description = "a";
+    test('Level', () {
+      expect(spell.level, equals({"Cleric": 1}));
+    });
+    spell.castingTime = "a";
+    test('CastingTime', () {
+      expect(spell.castingTime, equals("a"));
+    });
+    spell.components = ["a"];
+    test('Component', () {
+      expect(spell.components, equals(["a"]));
+    });
+    spell.range = "a";
+    test('Range', () {
+      expect(spell.range, equals("a"));
+    });
+    spell.target = "a";
+    test('Target', () {
+      expect(spell.target, equals("a"));
+    });
+    spell.duration = "a";
+    test('Duration', () {
+      expect(spell.duration, equals("a"));
+    });
   });
 }
