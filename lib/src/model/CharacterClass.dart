194dca6c7c127585168b40ac228816b598d70ab7
lou.broda <lou.broda@etu.uca.fr>
2023-03-03 21:56:45 +0100

    finish character details page + start spellparday switch case :memo:

--- lib/src/model/CharacterClass.dart
+++ lib/src/model/CharacterClass.dart
@@ -42,3 +42,57 @@ extension GetImage on CharacterClass {
   //   }
   //}
 }
+
+extension GetSpellPerDay on CharacterClass {
+  List<int> getListSpellPerDay(String cclass, int level) {
+    switch (cclass) {
+      case 'Bar':
+        switch (level) {
+          case 1:
+            return [1, 0, 0, 0, 0, 0, 0, 0, 0];
+          case 2:
+            return [2, 0, 0, 0, 0, 0, 0, 0, 0];
+          case 3:
+            return [3, 0, 0, 0, 0, 0, 0, 0, 0];
+          case 4:
+            return [3, 1, 0, 0, 0, 0, 0, 0, 0];
+          case 5:
+            return [4, 2, 0, 0, 0, 0, 0, 0, 0];
+          case 6:
+            return [4, 3, 0, 0, 0, 0, 0, 0, 0];
+          case 7:
+            return [4, 3, 1, 0, 0, 0, 0, 0, 0];
+          case 8:
+            return [4, 4, 2, 0, 0, 0, 0, 0, 0];
+          case 9:
+            return [5, 4, 3, 0, 0, 0, 0, 0, 0];
+          case 10:
+            return [5, 4, 3, 1, 0, 0, 0, 0, 0];
+          case 11:
+            return [5, 4, 4, 2, 0, 0, 0, 0, 0];
+          case 12:
+            return [5, 5, 4, 3, 0, 0, 0, 0, 0];
+          case 13:
+            return [5, 5, 4, 3, 1, 0, 0, 0, 0];
+          case 14:
+            return [5, 5, 4, 4, 2, 0, 0, 0, 0];
+          case 15:
+            return [5, 5, 5, 4, 3, 0, 0, 0, 0];
+          case 16:
+            return [5, 5, 5, 4, 3, 1, 0, 0, 0];
+          case 17:
+            return [5, 5, 5, 4, 4, 2, 0, 0, 0];
+          case 18:
+            return [5, 5, 5, 5, 4, 3, 0, 0, 0];
+          case 19:
+            return [5, 5, 5, 5, 5, 4, 0, 0, 0];
+          case 20:
+            return [5, 5, 5, 5, 5, 5, 0, 0, 0];
+          default:
+            return [];
+        }
+      default:
+        return [];
+    }
+  }
+}
