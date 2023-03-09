5b2df97ceacd9a73c9a9b10989faf845d4224d93
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-03-09 10:42:20 +0100

    :bento: réparation du problème d'asset, et ajout d'un padding autour de la table :art:

--- lib/src/view/spell_detail_page.dart
+++ lib/src/view/spell_detail_page.dart
@@ -18,51 +18,54 @@ class SpellDetailsPage extends StatelessWidget {
       //   child: Text(spell.description),
       // ),
       
-      body: Container(
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Table(
-        border: TableBorder.symmetric(
-          inside: BorderSide(width: 1.0, color: Colors.grey),
-          outside: BorderSide(width: 1.0, color: Colors.grey),
-        ),
-        children: [
-          const TableRow(
-            children: [
-              TableCell(
-                child: Text('Class'),
-              ),
-              TableCell(
-                child: Text('Niveau'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child:Container(
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3),
               ),
             ],
           ),
-          ...spell.GetMapClassLevel().entries.map(
-            (entry) => TableRow(
-              children: [
-                TableCell(
-                  child: Text(entry.key),
-                ),
-                TableCell(
-                  child: Text(entry.value.toString()),
-                ),
-              ],
+          child: Table(
+            border: TableBorder.symmetric(
+              inside: BorderSide(width: 1.0, color: Colors.grey),
+              outside: BorderSide(width: 1.0, color: Colors.grey),
             ),
+            children: [
+              const TableRow(
+                children: [
+                  TableCell(
+                    child: Text('Class'),
+                  ),
+                  TableCell(
+                    child: Text('Niveau'),
+                  ),
+                ],
+              ),
+              ...spell.GetMapClassLevel().entries.map(
+                (entry) => TableRow(
+                  children: [
+                    TableCell(
+                      child: Text(entry.key),
+                    ),
+                    TableCell(
+                      child: Text(entry.value.toString()),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ),
+        ),
+      )
     );
   }
 }
