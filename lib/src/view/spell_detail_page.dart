a4d131fa2cdecff9467d73f1fa60d280394c9623
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-03-09 16:48:22 +0100

    :art: continuation du front des details des sort, ajout de la description, tentative de création vaine d'un widget pour rendre le code plus propre :poop:

--- lib/src/view/spell_detail_page.dart
+++ lib/src/view/spell_detail_page.dart
@@ -1,6 +1,12 @@
-import '../model/spell.dart';
+//* Package *//
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+//* Model *//
+import '../model/spell.dart';
+
+//* Widgets *//
+import './widgets/tableSpellWidget.dart';
 
 class SpellDetailsPage extends StatelessWidget {
   final Spell spell;
@@ -23,76 +29,122 @@ class SpellDetailsPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 241, 117, 15),
       ),
-      // body: Center(
-      //   child: Text(spell.description),
-      // ),
-      
-      body: Wrap(
+      body: Wrap( //MAin wrap
         children: [
-          Padding(
-            padding: EdgeInsets.all(100.0),
-            child:Container(
-              padding: EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Table(
-                border: TableBorder.symmetric(
-                  inside: BorderSide(width: 1.0, color: Colors.grey),
+          
+          Container(
+            padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                children: [
-                  TableRow(
+            child: Text(
+              spell.description,
+              style:Theme.of(context).textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+            ),
+          ),
+    
+          Padding(
+              padding: EdgeInsets.all(100.0),
+              child:Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Wrap( //Wrap de toute la table
+                  children: [
+                    Center(
+                      child:Container(
+                        padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(15),
+                          )
+                        ),
+                        child:Text(
+                                "Table des classes",
+                                style:Theme.of(context).textTheme.titleLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                        )
+                    ),
+
+              //TableWidget(spell : this.spell)
+
+              Table(
+                    border: TableBorder.symmetric(
+                      inside: const BorderSide(
+                          width: 1.0, 
+                          color: Colors.grey
+                        ),
+                    ),
                     children: [
-                      TableCell(
-                        child: Text(
-                            'Class',
-                            style:Theme.of(context).textTheme.titleLarge,
-                            textAlign: TextAlign.center,
+                      TableRow(
+                        children: [
+                          TableCell(
+                            child: Text(
+                                'Class',
+                                style:Theme.of(context).textTheme.titleLarge,
+                                textAlign: TextAlign.center,
+                              ),
                           ),
+                          TableCell(
+                            child: Text(
+                                'Niveau',
+                                style:Theme.of(context).textTheme.titleLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                          ),
+                        ],
                       ),
-                      TableCell(
-                        child: Text(
-                            'Niveau',
-                            style:Theme.of(context).textTheme.titleLarge,
-                            textAlign: TextAlign.center,
-                          ),
+                      ...spell.GetMapClassLevel().entries.map(
+                        (entry) => TableRow(
+                          children: [
+                            TableCell(
+                              child: Text(
+                                entry.key,
+                                style:Theme.of(context).textTheme.bodyLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            TableCell(
+                              child: Text(
+                                entry.value.toString(),
+                                style:Theme.of(context).textTheme.bodyLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                  ...spell.GetMapClassLevel().entries.map(
-                    (entry) => TableRow(
-                      children: [
-                        TableCell(
-                          child: Text(
-                            entry.key,
-                            style:Theme.of(context).textTheme.bodyLarge,
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        TableCell(
-                          child: Text(
-                            entry.value.toString(),
-                            style:Theme.of(context).textTheme.bodyLarge,
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                ]  
               ),
-            ),
+            )
           )
-        ] 
+        ],
       )
     );
   }
