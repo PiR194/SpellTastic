3d14fdcb10539a5ff72609676e3c58f8979f5065
nicolas.franco <nicolas.franco@etu.uca.fr>
2023-03-11 05:53:07 +0100

    unecessary debug print removed :card_file_box:

--- lib/src/data/sqlite_data_strategy.dart
+++ lib/src/data/sqlite_data_strategy.dart
@@ -48,7 +48,6 @@ class SQLiteDataStrategy implements IDataStrategy {
           ? tmpName = list[i]['name']
           : tmpName = '';
 
-      print("debug: ${tmpName}");
       var tmpLevel = (list[i]['level'] != null && list[i]['level'] != Null)
           ? SpellSerializer.parseLevelAndGetClass(list[i]['level'])
           : <String, int>{};
