dce3cadb022e55a5181a156e63c4786cf86721dd
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-03-11 15:47:44 +0100

    :art:  Ajout de la scrollbar sur la page, ainsi que d'un widget avec la table de statistique d'un sort (comprenant duration, casting time, range et targe) :boom:

--- lib/src/view/spell_detail_page.dart
+++ lib/src/view/spell_detail_page.dart
@@ -1,4 +1,5 @@
 //* Package *//
+import 'package:code/src/view/widgets/StatTable.dart';
 import 'package:flutter/cupertino.dart';
 import '../model/spell.dart';
 import 'package:flutter/material.dart';
@@ -21,150 +22,140 @@ class SpellDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-              spell.name,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 35,
-                fontFamily: 'Anaktoria',
-                fontWeight: FontWeight.bold),
-            ),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 241, 117, 15),
-      ),
-      body: Wrap( //MAin wrap
-        children: [
-          
-          Container(
-            padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
+        appBar: AppBar(
+          title: Text(
+                spell.name,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-            child: RichText(
-              text: TextSpan(
-                text: 'Description du sort : \n',
-                style: Theme.of(context).textTheme.headlineLarge,
-                children: <TextSpan>[
-                  TextSpan(
-                    text: '',
-                    children: [
-                      WidgetSpan(
-                        child: HtmlWidget(
-                          spell.description,
-                          textStyle: Theme.of(context).textTheme.bodyMedium
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  fontSize: 35,
+                  fontFamily: 'Anaktoria',
+                  fontWeight: FontWeight.bold),
               ),
-            ),
-          ),
-    
-          Padding(
-              padding: EdgeInsets.all(100.0),
-              child:Container(
-                padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 241, 117, 15),
+        ),
+        body: SingleChildScrollView(
+          child:Wrap( //MAin wrap
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 100.0,
+                  right: 100.0,
                 ),
-                child: Wrap( //Wrap de toute la table
-                  children: [
-                    Center(
-                      child:Container(
-                        padding: EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
+                child:Container( //* Description
+                  padding: EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),
                           ),
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(15),
-                          )
-                        ),
-                        child:Text(
-                                "Table des classes",
-                                style:Theme.of(context).textTheme.titleLarge,
-                                textAlign: TextAlign.center,
+                        ],
+                      ),
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Description du sort : \n',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '',
+                          children: [
+                            WidgetSpan(
+                              child: HtmlWidget(
+                                spell.description,
+                                textStyle: Theme.of(context).textTheme.bodyMedium
                               ),
-                        )
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-
-                    TableWidget(spell : spell)
-
-              // Table(
-              //       border: TableBorder.symmetric(
-              //         inside: const BorderSide(
-              //             width: 1.0, 
-              //             color: Colors.grey
-              //           ),
-              //       ),
-              //       children: [
-              //         TableRow(
-              //           children: [
-              //             TableCell(
-              //               child: Text(
-              //                   'Class',
-              //                   style:Theme.of(context).textTheme.titleLarge,
-              //                   textAlign: TextAlign.center,
-              //                 ),
-              //             ),
-              //             TableCell(
-              //               child: Text(
-              //                   'Niveau',
-              //                   style:Theme.of(context).textTheme.titleLarge,
-              //                   textAlign: TextAlign.center,
-              //                 ),
-              //             ),
-              //           ],
-              //         ),
-              //         ...spell.GetMapClassLevel().entries.map(
-              //           (entry) => TableRow(
-              //             children: [
-              //               TableCell(
-              //                 child: Text(
-              //                   entry.key,
-              //                   style:Theme.of(context).textTheme.bodyLarge,
-              //                   textAlign: TextAlign.center,
-              //                 ),
-              //               ),
-              //               TableCell(
-              //                 child: Text(
-              //                   entry.value.toString(),
-              //                   style:Theme.of(context).textTheme.bodyLarge,
-              //                   textAlign: TextAlign.center,
-              //                 ),
-              //               ),
-              //             ],
-              //           ),
-              //         ),
-              //       ],
-              //     ),
-                ]  
+                  ),
+                )
               ),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 15.0,
+                  bottom: 15.0,
+                  left: 100.0,
+                  right: 100.0,
+                ),
+                child: Center(
+                  child: Container( //* Description
+                    padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                    child: StatTable(spell : spell),
+                  )
+                )
+              ),
+
+              Padding(
+                  padding: const EdgeInsets.only(
+                    top: 15.0,
+                    bottom: 15.0,
+                    left: 100.0,
+                    right: 100.0,
+                  ),
+                  child:Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Wrap( //*Wrap de toute la table
+                      children: [
+                        Center(
+                          child:Container(
+                            padding: EdgeInsets.all(16.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(15),
+                              )
+                            ),
+                            child:Text(
+                                    "Table des classes",
+                                    style:Theme.of(context).textTheme.titleLarge,
+                                    textAlign: TextAlign.center,
+                                  ),
+                            )
+                        ),
+                        TableWidget(spell : spell),
+                        const SizedBox(height: 16.0),
+                      ]  
+                    ),
+                  )
+                )
+              ],
             )
           )
-        ],
-      )
     );
   }
 }
